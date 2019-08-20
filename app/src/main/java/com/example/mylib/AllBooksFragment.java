a8e8f637a28743a52a2049cdf278e546ba65e03a
adas3000 <youngtoss38@gmail.com>
2019-08-20 13:45:56 +0200

    added downloading images from URL

--- app/src/main/java/com/example/mylib/AllBooksFragment.java
+++ app/src/main/java/com/example/mylib/AllBooksFragment.java
@@ -1,7 +1,10 @@
 package com.example.mylib;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
@@ -12,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,7 +24,9 @@ import androidx.fragment.app.Fragment;
 import com.example.mylib.Data.Book;
 import com.example.mylib.Img.ImageDownloader;
 import com.example.mylib.sql.SqlManager;
+import com.squareup.picasso.Picasso;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class AllBooksFragment extends Fragment {
@@ -40,16 +46,26 @@ public class AllBooksFragment extends Fragment {
         sqlManager = new SqlManager(context);
         ArrayList<Book> bookArrayList = sqlManager.getValues(false);
 
-        RelativeLayout relativeLayout = view.findViewById(R.id.relativelayout);
+        TextView textView = view.findViewById(R.id.textView_Book);
+        ImageView imageView = view.findViewById(R.id.imageView_Book);
 
-        for(Book b : bookArrayList){
-            TextView tv = new TextView(context);
-            tv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            tv.setTextSize(20);
-            tv.setTextColor(Color.BLACK);
-            tv.setText(b.getAuthor()+"\n"+b.getTitle());
-            relativeLayout.addView(tv);
-        }
+        textView.setText(bookArrayList.get(0).getAuthor() + "\n" + bookArrayList.get(0).getTitle());
+
+        String URL = "https://image.ceneostatic.pl/data/products/9367217/49f352e8-fe40-4a0a-b2c5-f38ba07d1d3d_i-harry-potter-i-komnata-tajemnic-harry-potter-and-the-chamber-of-secrets-3dvd.jpg";
+
+        Picasso.with(getContext()).load(URL).into(imageView,new com.squareup.picasso.Callback(){
+
+
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getContext(),"Image loaded",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError() {
+                Toast.makeText(getContext(),"cannot load image",Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
@@ -57,7 +73,33 @@ public class AllBooksFragment extends Fragment {
         return view;
     }
 
+    public class GetImageFromURL extends AsyncTask<String, Void, Bitmap> {
+        ImageView imgV;
+        Bitmap bitmap;
 
+        public GetImageFromURL(ImageView imgV, Bitmap bitmap) {
+            this.imgV = imgV;
+            this.bitmap = bitmap;
+        }
 
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String url = strings[0];
+            bitmap = null;
+            try {
+                InputStream in = new java.net.URL(url).openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.fillInStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imgV.setImageBitmap(bitmap);
+        }
+    }
 
 }
