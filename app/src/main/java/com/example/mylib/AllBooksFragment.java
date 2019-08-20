90d066e061cff111c57c60d202a0f044ba3395b1
adas3000 <youngtoss38@gmail.com>
2019-08-20 13:48:56 +0200

    Removed useless imageDownloader class

--- app/src/main/java/com/example/mylib/AllBooksFragment.java
+++ app/src/main/java/com/example/mylib/AllBooksFragment.java
@@ -53,53 +53,10 @@ public class AllBooksFragment extends Fragment {
 
         String URL = "https://image.ceneostatic.pl/data/products/9367217/49f352e8-fe40-4a0a-b2c5-f38ba07d1d3d_i-harry-potter-i-komnata-tajemnic-harry-potter-and-the-chamber-of-secrets-3dvd.jpg";
 
-        Picasso.with(getContext()).load(URL).into(imageView,new com.squareup.picasso.Callback(){
-
-
-            @Override
-            public void onSuccess() {
-                Toast.makeText(getContext(),"Image loaded",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError() {
-                Toast.makeText(getContext(),"cannot load image",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
+        Picasso.with(getContext()).load(URL).into(imageView);
 
 
         return view;
     }
-
-    public class GetImageFromURL extends AsyncTask<String, Void, Bitmap> {
-        ImageView imgV;
-        Bitmap bitmap;
-
-        public GetImageFromURL(ImageView imgV, Bitmap bitmap) {
-            this.imgV = imgV;
-            this.bitmap = bitmap;
-        }
-
-        @Override
-        protected Bitmap doInBackground(String... strings) {
-            String url = strings[0];
-            bitmap = null;
-            try {
-                InputStream in = new java.net.URL(url).openStream();
-                bitmap = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                e.fillInStackTrace();
-            }
-            return bitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            imgV.setImageBitmap(bitmap);
-        }
-    }
-
 }
+
