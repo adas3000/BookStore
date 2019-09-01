eb4684b55fdce904a48ccdf7a559be82bf2e4f51
adas3000 <youngtoss38@gmail.com>
2019-09-01 12:18:57 +0200

    sqlandroidTest merge fixed

--- app/src/androidTest/java/com/example/mylib/SqlAndroidTest.java
+++ app/src/androidTest/java/com/example/mylib/SqlAndroidTest.java
@@ -1,8 +1,6 @@
 package com.example.mylib;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 
 import androidx.test.InstrumentationRegistry;
 
@@ -32,7 +30,7 @@ public class SqlAndroidTest {
     }
     @Before
     public void clearDb(){
-        
+
     }
 
     @Test
@@ -41,21 +39,10 @@ public class SqlAndroidTest {
     }
 
     @Test
-<<<<<<< HEAD
-    public void sqlTest(){
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        Bitmap bmp = BitmapFactory.decodeFile("@drawable/", options);
-
-
-        sqlManager.addBookToDb("Hobbit","Tolkien","Two hobbits went somewhere",
-              bmp,0,2013,10,15 );
-=======
     public void sqlTest() {
 
         sqlManager.addBookToDb("Hobbit", "Tolkien", "Two hobbits went somewhere",
                 "drawable/photos/Hobbit1.png", 0, 2013, 10, 15);
->>>>>>> itemadapter_Branch
 
         ArrayList<Book> bookArrayList = sqlManager.getValues();
         assertEquals(0, bookArrayList.size());
@@ -65,43 +52,17 @@ public class SqlAndroidTest {
 
 
     @Test
-<<<<<<< HEAD
-    public void areValuesGoodTest(){
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        Bitmap bmp = BitmapFactory.decodeFile("@drawable/ic_books.xml", options);
-
-        sqlManager.addBookToDb("Hobbit","Tolkien","Two hobbits went somewhere",
-                bmp,0,2013,10,15 );
-        sqlManager.addBookToDb("Harry Potter","Rowling","Story of Big Wizard",
-                bmp,1,2019,8,17 );
-=======
     public void areValuesGoodTest() {
         sqlManager.addBookToDb("Hobbit", "Tolkien", "Two hobbits went somewhere",
                 "drawable/photos/Hobbit1.png", 0, 2013, 10, 15);
         sqlManager.addBookToDb("Harry Potter", "Rowling", "Story of Big Wizard",
                 "drawable/photos/Harry1.png", 1, 2019, 8, 17);
->>>>>>> itemadapter_Branch
 
         ArrayList<Book> bookArrayList = sqlManager.getValues();
 
         assertEquals(2, bookArrayList.size());
 
         //book 1
-<<<<<<< HEAD
-        assertEquals("Tolkien",bookArrayList.get(0).getAuthor());
-        assertEquals("Hobbit",bookArrayList.get(0).getTitle());
-        assertEquals("Two hobbits went somewhere",bookArrayList.get(0).getShort_description());
-        assertEquals(false,bookArrayList.get(0).isReadenByUser());
-        //book 2
-        assertEquals("Rowling",bookArrayList.get(1).getAuthor());
-        assertEquals("Harry Potter",bookArrayList.get(1).getTitle());
-        assertEquals("Story of Big Wizard",bookArrayList.get(1).getShort_description());
-        assertEquals(true,bookArrayList.get(1).isReadenByUser());
-        assertEquals(2019,bookArrayList.get(1).getFinish_date().year);
-        assertEquals(8,bookArrayList.get(1).getFinish_date().month);
-        assertEquals(17,bookArrayList.get(1).getFinish_date().day);
-=======
         assertEquals("Tolkien", bookArrayList.get(0).getAuthor());
         assertEquals("Hobbit", bookArrayList.get(0).getTitle());
         assertEquals("Two hobbits went somewhere", bookArrayList.get(0).getShort_description());
@@ -113,7 +74,6 @@ public class SqlAndroidTest {
         assertEquals("Story of Big Wizard", bookArrayList.get(1).getShort_description());
         assertEquals("drawable/photos/Harry1.png", bookArrayList.get(1).getImage_url());
         assertEquals(true, bookArrayList.get(1).isReadenByUser());
->>>>>>> itemadapter_Branch
     }
 
     @Test
